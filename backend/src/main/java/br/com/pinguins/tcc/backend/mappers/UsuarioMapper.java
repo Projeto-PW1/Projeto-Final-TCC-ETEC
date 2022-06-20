92076c3b6c9e4780900469c57e96bc4b03d80a82
Pablo Junior <pablojuniorgn2@gmail.com>
2022-06-20 19:45:03 -0300

    edit mappers

--- backend/src/main/java/br/com/pinguins/tcc/backend/mappers/UsuarioMapper.java
+++ backend/src/main/java/br/com/pinguins/tcc/backend/mappers/UsuarioMapper.java
@@ -12,19 +12,22 @@ public class UsuarioMapper {
 
     public Usuario toEntity(UsuarioDTO dto) {
         Usuario usuario = new Usuario();
-
+        usuario.setId(dto.getId());
         usuario.setNome(dto.getNome());
         usuario.setLogin(dto.getLogin());
         usuario.setSenha(dto.getSenha());
+        usuario.setRemedios(dto.getRemedios());
         return usuario;
     }
 
     public UsuarioDTO toDto(Usuario usuario) {
         UsuarioDTO dto = new UsuarioDTO();
 
+        dto.setId(usuario.getId());
         dto.setNome(usuario.getNome());
         dto.setLogin(usuario.getLogin());
         dto.setSenha(usuario.getSenha());
+        dto.setRemedios(usuario.getRemedios());
         return dto;
     }
 

07b7bbb221ea5c7436c67b281e6782b23ab376b3
Pablo Junior <pablojuniorgn2@gmail.com>
2022-06-20 19:42:54 -0300

    edit dtos

--- backend/src/main/java/br/com/pinguins/tcc/backend/dtos/UsuarioDTO.java
+++ backend/src/main/java/br/com/pinguins/tcc/backend/dtos/UsuarioDTO.java
@@ -1,12 +1,16 @@
 package br.com.pinguins.tcc.backend.dtos;
 
+import br.com.pinguins.tcc.backend.entities.Remedio;
 import br.com.pinguins.tcc.backend.entities.Usuario;
 
 import javax.validation.constraints.Email;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UsuarioDTO {
 
 
+    private Integer id;
     private String nome;
 
     @Email
@@ -14,12 +18,23 @@ public class UsuarioDTO {
 
     private String senha;
 
+    private Set<Remedio> remedios = new HashSet<>();
+
     public UsuarioDTO(){}
 
     public UsuarioDTO(Usuario usuario) {
         nome = usuario.getNome();
         login = usuario.getLogin();
         senha = usuario.getSenha();
+        remedios = usuario.getRemedios();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -44,5 +59,13 @@ public class UsuarioDTO {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public void setRemedios(Set<Remedio> remedios) {
+        this.remedios = remedios;
+    }
+
+    public Set<Remedio> getRemedios() {
+        return remedios;
     }
 }

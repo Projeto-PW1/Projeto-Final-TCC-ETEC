8708e7f689835b8683da17a9c58bff74bff6dfb5
pablojg9 <pablojuniorgn2@gmail.com>
2022-06-21 13:07:53 -0300

    add delete

--- backend/src/main/java/br/com/pinguins/tcc/backend/repositories/RemedioRepository.java
+++ backend/src/main/java/br/com/pinguins/tcc/backend/repositories/RemedioRepository.java
@@ -2,6 +2,7 @@ package br.com.pinguins.tcc.backend.repositories;
 
 import br.com.pinguins.tcc.backend.entities.Remedio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,8 @@ public interface RemedioRepository extends JpaRepository<Remedio, Integer> {
 
     @Query("SELECT n FROM Remedio as n WHERE n.titulo LIKE %?1")
     List<Remedio> findByNome(String nome);
+
+    @Modifying
+    @Query("delete from Remedio u where u.id in ?1")
+    void deleteRemedioByWithId(Integer id);
 }
